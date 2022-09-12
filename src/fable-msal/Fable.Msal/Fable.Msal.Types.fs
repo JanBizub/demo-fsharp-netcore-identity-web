8486acd80aae1bf2c529d402fbd7d96c1a07e65f
Jan Bizub <jan.bizub@outlook.com>
2022-09-12 14:48:48 +0200

    work

--- src/fable-msal/Fable.Msal/Fable.Msal.Types.fs
+++ src/fable-msal/Fable.Msal/Fable.Msal.Types.fs
@@ -2,8 +2,6 @@
 
 open System
 open System.Collections.Generic
-open Fable.Core.JS
-open Fable.Core
 
 type AccountInfo =
     { homeAccountId: string
@@ -62,73 +60,20 @@ type AzureCloudInstance =
     | [<CompiledName("https://login.microsoftonline.de")>] AzureGermany
     | [<CompiledName("https://login.microsoftonline.us")>] AzureUsGovernment
 
-type AzureCloudOptions =
-    { azureCloudInstance: AzureCloudInstance
-      tenant: string option }
-
-type BaseAuthRequest =
-    abstract authority: string
-    abstract correlationId: string
-    abstract scopes: string array
-    abstract authenticationScheme: AuthenticationScheme option
-    abstract claims: string option
-    abstract shrClaims: string option
-    abstract shrNonce: string option
-    abstract resourceRequestMethod: string option
-    abstract resourceRequestUri: string option
-    abstract sshJwk: string option
-    abstract sshKid: string option
-    abstract azureCloudOptions: AzureCloudOptions option
-    abstract requestedClaimsHash: string option
-
 type ResponseMode =
     | [<CompiledName("query")>] QUERY
     | [<CompiledName("fragment")>] FRAGMENT
     | [<CompiledName("form_post")>] FORM_POST
 
-type CommonAuthorizationUrlRequest =
-    inherit BaseAuthRequest
+type AzureCloudOptions =
+    { azureCloudInstance: AzureCloudInstance
+      tenant: string option }
 
-    abstract redirectUri: string
-    abstract responseMode: ResponseMode
-    abstract account: AccountInfo option
-    abstract codeChallenge: string option
-    abstract codeChallengeMethod: string option
-    abstract domainHint: string option with get, set
-    abstract extraQueryParameters: Dictionary<string, string>
-    abstract tokenQueryParameters: Dictionary<string, string>
-    abstract extraScopesToConsent: string array
-    abstract loginHint: string option
-    abstract nonce: string option
-    abstract prompt: string option
-    abstract sid: string option
-    abstract state: string option
-    abstract nativeBroker: bool option
-
-// type RedirectRequest =
-//   inherit CommonAuthorizationUrlRequest
-//
-//   abstract scopes: string array
-//   abstract redirectStartPage: string option
-// onRedirectNavigate?: (url: string) => boolean | void;
-
-/// SilentRequest: Request object passed by user to retrieve tokens from the
-/// cache, renew an expired token with a refresh token, or retrieve a code (first leg of authorization code grant flow)
-/// in a hidden iframe.
-///
-/// - scopes                 - Array of scopes the application is requesting access to.
-/// - authority              - Url of the authority which the application acquires tokens from.
-/// - correlationId          - Unique GUID set per request to trace a request end-to-end for telemetry purposes.
-/// - account                - Account entity to lookup the credentials.
-/// - forceRefresh           - Forces silent requests to make network calls if true.
-/// - extraQueryParameters   - String to string map of custom query parameters added to the /authorize call. Only used when renewing the refresh token.
-/// - tokenQueryParameters   - String to string map of custom query parameters added to the /token call. Only used when renewing access tokens.
-/// - redirectUri            - The redirect URI where authentication responses can be received by your application. It must exactly match one of the redirect URIs registered in the Azure portal. Only used for cases where refresh token is expired.
 type SilentRequest =
     { account: AccountInfo option
       scopes: string list
       authority: string option
-      correlationId: Guid option // todo: lepší než originál. V Typescriptu je tady string.
+      correlationId: Guid option
       forceRefresh: bool option
       extraQueryParameters: Dictionary<string, string> option }
 
@@ -147,15 +92,3 @@ type RedirectRequest = { prompt: string; scopes: string list }
 [<RequireQualifiedAccess>]
 module RedirectRequest =
     let empty () = { prompt = ""; scopes = [] }
-
-[<AllowNullLiteral>]
-type IPublicClientApplication =
-    abstract acquireTokenSilent: silentRequest: SilentRequest -> Promise<AuthenticationResult>
-    abstract getAccountByHomeId: homeAccountId: string -> AccountInfo option
-    abstract getAccountByLocalId: localId: string -> AccountInfo option
-    abstract getAccountByUsername: userName: string -> AccountInfo option
-    abstract getAllAccounts: unit -> AccountInfo array
-    abstract handleRedirectPromise: ?hash: string -> Promise<AuthenticationResult option>
-    abstract loginRedirect: ?request: RedirectRequest -> Promise<unit>
-    abstract setActiveAccount: account: AccountInfo option -> unit
-    abstract getActiveAccount: unit -> AccountInfo option
---
+++ src/fable-msal/Fable.Msal/Interfaces.fs
@@ -0,0 +1,62 @@
+﻿module internal Fable.Msal.Interfaces
+
+open Fable.Core
+open System.Collections.Generic
+
+type IBaseAuthRequest =
+    abstract authority: string
+    abstract correlationId: string
+    abstract scopes: string array
+    abstract authenticationScheme: AuthenticationScheme option
+    abstract claims: string option
+    abstract shrClaims: string option
+    abstract shrNonce: string option
+    abstract resourceRequestMethod: string option
+    abstract resourceRequestUri: string option
+    abstract sshJwk: string option
+    abstract sshKid: string option
+    abstract azureCloudOptions: AzureCloudOptions option
+    abstract requestedClaimsHash: string option
+
+
+type ICommonAuthorizationUrlRequest =
+    inherit IBaseAuthRequest
+
+    abstract redirectUri: string
+    abstract responseMode: ResponseMode
+    abstract account: AccountInfo option
+    abstract codeChallenge: string option
+    abstract codeChallengeMethod: string option
+    abstract domainHint: string option with get, set
+    abstract extraQueryParameters: Dictionary<string, string>
+    abstract tokenQueryParameters: Dictionary<string, string>
+    abstract extraScopesToConsent: string array
+    abstract loginHint: string option
+    abstract nonce: string option
+    abstract prompt: string option
+    abstract sid: string option
+    abstract state: string option
+    abstract nativeBroker: bool option
+
+
+type ICommonSilentFlowRequest =
+    inherit IBaseAuthRequest
+    abstract account: AccountInfo
+    abstract forceRefresh: bool
+    abstract tokenQueryParameters: Dictionary<string, string> option
+
+
+type ISilentRequest =
+    inherit ICommonSilentFlowRequest
+    abstract redirectUri: string option
+    abstract extraQueryParameters: Dictionary<string, string> option
+    abstract authority: string option
+    abstract account: AccountInfo option
+    abstract correlationId: string option
+    abstract forceRefresh: bool option
+
+
+type IRedirectRequest =
+    abstract scopes: string array
+    abstract redirectStartPage: string option
+    abstract onRedirectNavigate: (string -> U2<bool, unit>) option
